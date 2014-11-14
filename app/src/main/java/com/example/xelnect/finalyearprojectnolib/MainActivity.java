73ddd28b0ff26bf930bb47a60b54160661d5db38
Xelnect <dido.jordanov7@gmail.com>
2014-11-14 02:10:11 +0200

    gvyvyu

--- app/src/main/java/com/example/xelnect/finalyearprojectnolib/MainActivity.java
+++ app/src/main/java/com/example/xelnect/finalyearprojectnolib/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.xelnect.finalyearprojectnolib;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -12,10 +17,12 @@ import java.io.File;
 
 public class MainActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -47,5 +54,40 @@ public class MainActivity extends Activity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+    public static int loadTexture(final Context context, final int resourceId)
+    {
+        final int[] textureHandle = new int[1];
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+
+        if (textureHandle[0] != 0)
+        {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inScaled = false;   // No pre-scaling
+
+            // Read in the resource
+            final Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId, options);
+
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+            // Recycle the bitmap, since its data has been loaded into OpenGL.
+            bitmap.recycle();
+        }
+
+        if (textureHandle[0] == 0)
+        {
+            throw new RuntimeException("Error loading texture.");
+        }
+
+        return textureHandle[0];
     }
 }
---
+++ app/src/main/java/com/example/xelnect/finalyearprojectnolib/TextureDrawing.java
@@ -0,0 +1,8 @@
+package com.example.xelnect.finalyearprojectnolib;
+
+/**
+ * Created by XelnectPC on 11/14/2014.
+ */
+public class TextureDrawing {
+
+}
